87262fc9d5e1757390433690051cc65ee1251633
vonavi <ivvl82@gmail.com>
2013-04-14 15:41:08 +0300

    Just fix indentation in "init.el" by combo "M-;".

--- init.el
+++ init.el
@@ -37,10 +37,10 @@
 ;;-----------------------
 
 (add-to-list 'default-frame-alist '(font . "Consolas 14")) ; set default font
-(blink-cursor-mode 0) ; no blinking cursor
-(setq inhibit-splash-screen t) ; prevent silly initial splash screen
-(tool-bar-mode 0) ; no tool bar
-(set-scroll-bar-mode 'right) ; place scroll bar
+(blink-cursor-mode 0)            ; no blinking cursor
+(setq inhibit-splash-screen t)   ; prevent silly initial splash screen
+(tool-bar-mode 0)                ; no tool bar
+(set-scroll-bar-mode 'right)     ; place scroll bar
 
 ;; Install a chosen color theme
 (require 'color-theme)
@@ -57,10 +57,10 @@
                     (getenv "HOME") "~" (or buffer-file-name "%b"))))))
 
 ;; Mode bar preferences
-(column-number-mode 1) ; show column number in mode-line
-(setq display-time-day-and-date t            ; display the day and date in the mode line
-      display-time-24hr-format t             ; use 24hr format
-      display-time-interval 10               ; redisplay every ten seconds
+(column-number-mode 1)            ; show column number in mode-line
+(setq display-time-day-and-date t ; display the day and date in the mode line
+      display-time-24hr-format t  ; use 24hr format
+      display-time-interval 10    ; redisplay every ten seconds
       display-time-default-load-average nil) ; don't display the system load average
 (display-time)
 
@@ -70,29 +70,29 @@
 
 ;; Enable mouse wheel
 (mouse-wheel-mode 1)
-(setq mouse-wheel-scroll-amount '(1)    ; mouse scroll one line at a time
+(setq mouse-wheel-scroll-amount '(1) ; mouse scroll one line at a time
       mouse-wheel-progressive-speed nil ; don't accelerate scrolling
       mouse-wheel-follow-mouse t        ; scroll window under mouse
-      setscroll-step 1                  ; keyboard scroll one line at a time
-      scroll-conservatively 10000       ; scroll one line at a time if point moves off-screen
+      setscroll-step 1            ; keyboard scroll one line at a time
+      scroll-conservatively 10000 ; scroll one line at a time if point moves off-screen
       scroll-preserve-screen-position t ; keep point at the same screen position
       scroll-margin 0                   ; set scroll margin
-      auto-window-vscroll nil)          ; don't adjust window-vscroll to view tall lines
+      auto-window-vscroll nil) ; don't adjust window-vscroll to view tall lines
 
-(setq x-select-enable-clipboard t) ; cut and paste to the X clipboard
-(setq mouse-yank-at-point t) ; paste at point NOT at cursor
+(setq x-select-enable-clipboard t)  ; cut and paste to the X clipboard
+(setq mouse-yank-at-point t)        ; paste at point NOT at cursor
 
 ;;-------------------------
 ;; Backup and restore Emacs
 ;;-------------------------
 
 ;; Create a backup file
-(setq backup-by-copying t                                    ; don't clobber symlinks
+(setq backup-by-copying t               ; don't clobber symlinks
       backup-directory-alist '(("." . "~/.emacs.d/backup/")) ; don't litter my fs tree
-      kept-new-versions 4                                    ; keep 4 last backups
-      kept-old-versions 0                                    ; don't keep first backups
-      delete-old-versions t                                  ; delete intermediate backup files
-      version-control t)                                     ; use versioned backups
+      kept-new-versions 4           ; keep 4 last backups
+      kept-old-versions 0           ; don't keep first backups
+      delete-old-versions t         ; delete intermediate backup files
+      version-control t)            ; use versioned backups
 
 ;; Automatically save and restore sessions
 (desktop-save-mode 1)
@@ -124,13 +124,13 @@
 ;; Do interactively things with buffers and files
 (ido-mode 1)
 (setq ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
-      ido-ignore-buffers '("\\` " "\\`\\*")   ; ignore these buffers
-      ido-confirm-unique-completion t         ; wait for RET with unique completion
+      ido-ignore-buffers '("\\` " "\\`\\*") ; ignore these buffers
+      ido-confirm-unique-completion t ; wait for RET with unique completion
       confirm-nonexistent-file-or-buffer nil) ; don't need confirmation
 
-(icomplete-mode 1) ; completion in mini-buffer
+(icomplete-mode 1)           ; completion in mini-buffer
 (setq resize-mini-windows t) ; minibuffer gets resized if it becomes too big
-(fset 'yes-or-no-p 'y-or-n-p) ; use y or n instead of yes or not
+(fset 'yes-or-no-p 'y-or-n-p)       ; use y or n instead of yes or not
 
 ;;---------------------
 ;; Emacs buffer editing
@@ -154,10 +154,10 @@
 
 ;; Indentation setup
 (setq-default indent-tabs-mode nil) ; never use tab characters for indentation
-(setq tab-width 4          ; set tab-width
-      c-basic-offset 2     ; indentation level in CC mode
-      js-indent-level 2    ; indentation level in JS mode
-      css-indent-offset 2) ; indentation level in CSS mode
+(setq tab-width 4                   ; set tab-width
+      c-basic-offset 2              ; indentation level in CC mode
+      js-indent-level 2             ; indentation level in JS mode
+      css-indent-offset 2)          ; indentation level in CSS mode
 
 ;; A single <DEL> or <BS> command deletes a contiguous block of whitespace
 (require 'hungry-delete)
@@ -168,11 +168,11 @@
 ;;--------------------
 
 (setq default-input-method "russian-computer") ; default input method
-(show-paren-mode 1) ; highlight parenthesis pairs
+(show-paren-mode 1)                      ; highlight parenthesis pairs
 (setq blink-matching-paren-distance nil) ; search backwards for matching open-paren
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on) ; translate escape sequences
-(setq calendar-week-start-day 1      ; week starts Monday
-      calendar-date-style 'european) ; European style calendar
+(setq calendar-week-start-day 1         ; week starts Monday
+      calendar-date-style 'european)    ; European style calendar
 
 ;; This function reverts all buffers that are visiting a file.
 (defun revert-all-buffers ()
