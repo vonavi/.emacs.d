94769f04fe3c88abb83d91169941ebd8408331ca
vonavi <ivvl82@gmail.com>
2013-09-12 10:45:41 +0300

    Org-rc: remove a commentary part and keep a trailing slash in `org-protocol-store-link'.

--- rc/org-rc.el
+++ rc/org-rc.el
@@ -4,32 +4,9 @@
 
 ;; Author: Vladimir Ivanov <ivvl82@gmail.com>
 
-;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 
 ;;; Code:
-
-(defadvice org-protocol-store-link (before rstrip-link activate)
-  "Force `org-protocol-store-link' to store the URL without
-trailing slash \"/\" and \"/index.output_ext\". Here, \"%2F\" is
-the URL-encoded symbol of slash."
-  (ad-set-arg 0 (replace-regexp-in-string
-                 "%2F\\(index\\.[a-zA-Z]+\\)?/" "/"
-                 (ad-get-arg 0))))
 
 ;; Set Jekyll’s handler for org-protocol
 (require 'org-protocol-jekyll)
