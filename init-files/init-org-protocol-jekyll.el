bd9867f32c4301bb551d059c3d4f19841ae969e5
vonavi <ivvl82@gmail.com>
2014-08-04 18:40:16 +0300

    Move 'org-protocol-jekyll' init file into 'init-files' directory.

--- init-files/init-org-protocol-jekyll.el
+++ init-files/init-org-protocol-jekyll.el
@@ -1,6 +1,6 @@
-;;; org-rc.el ---
+;;; init-org-protocol-jekyll.el ---
 
-;; Copyright (C) 2013  Vladimir Ivanov
+;; Copyright (C) 2014  Vladimir Ivanov
 
 ;; Author: Vladimir Ivanov <ivvl82@gmail.com>
 ;; Keywords:
@@ -11,18 +11,18 @@
 
 ;;; Code:
 
-;; Set Jekyll’s handler for org-protocol
+;; Set Jekyll's handler for org-protocol
 (require 'org-protocol-jekyll)
 (setq org-protocol-jekyll-alist
       '(("Локальная версия сайта"
          :base-url "http://localhost:4000"
          :permalink "pretty"
-         :working-directory "~/Dropbox/openshift/jekyll"
+         :working-directory "~/openshift/jekyll"
          :working-suffix ".org")
         ("Редактор на стероидах"
          :base-url "http://www.vonavi.me"
          :permalink "pretty"
-         :working-directory "~/Dropbox/openshift/jekyll"
+         :working-directory "~/openshift/jekyll"
          :working-suffix ".org")))
 
-;;; org-rc.el ends here
+;;; init-org-protocol-jekyll.el ends here
