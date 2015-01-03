2075e41b3f04652084eb70611a528d3d4a041859
vonavi <ivvl82@gmail.com>
2015-01-04 01:40:17 +0300

    Update the Marmalade URL.

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
 (add-to-list 'package-archives
              '("elpa" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
