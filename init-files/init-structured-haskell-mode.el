1979382669e1004944edb4bb6f9a5687e821ddb4
vonavi <ivvl82@gmail.com>
2015-02-08 11:10:12 +0300

    Don't use indentation modes with Structured Haskell mode.

--- init-files/init-structured-haskell-mode.el
+++ init-files/init-structured-haskell-mode.el
@@ -14,6 +14,9 @@
 (require 'shm)
 (add-hook 'haskell-mode-hook 'structured-haskell-mode)
 
+;; Structured Haskell mode is incompatible with indentation modes
+(remove-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
 ;; The following are apparently pretty good for solarized-light
 (set-face-background 'shm-current-face "#eee8d5")
 (set-face-background 'shm-quarantine-face "lemonchiffon")
