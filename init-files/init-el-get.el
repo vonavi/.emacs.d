250bb69a2291e399e16def49164e0b0da9ad5a38
Vladimir Ivanov <ivvl82@gmail.com>
2021-01-18 22:59:49 +0300

    Install Pomidor, an implementation of http://www.pomodorotechnique.com.

--- init-files/init-el-get.el
+++ init-files/init-el-get.el
@@ -21,16 +21,17 @@
     adaptive-wrap           ; smart line-wrapping with wrap-prefix
     auctex                  ; integrated environment for *TeX*
     essh    ; a set of commands that emulate for bash what ESS is to R
-    fill-column-indicator    ; graphically indicate the fill column
-    fullscreen               ; full screen
-    hungry-delete            ; hungry delete minor mode
-    magit                    ; a Git porcelain inside Emacs
-    nlinum                   ; show line numbers in the margin
+    fill-column-indicator       ; graphically indicate the fill column
+    fullscreen                  ; full screen
+    hungry-delete               ; hungry delete minor mode
+    magit                       ; a Git porcelain inside Emacs
+    nlinum                      ; show line numbers in the margin
     org     ; outline-based notes management and organizer
+    pomidor ; simple and cool http://www.pomodorotechnique.com timer
     realgud ; a modular front-end for interacting with external debuggers
     sdcv    ; interface for sdcv (StartDict console version)
-    solarized-emacs          ; Solarized for Emacs
-    undo-tree                ; treat undo history as a tree
+    solarized-emacs  ; Solarized for Emacs
+    undo-tree        ; treat undo history as a tree
     window-margin    ; automatic margins for visual-line-mode wrapping
     ws-butler)       ; unobtrusively remove trailing whitespace
   "A list of packages to ensure are installed at launch.")
---
+++ init-files/init-pomidor.el
@@ -0,0 +1,18 @@
+;;; init-pomidor.el ---
+
+;; Copyright (C) 2021  Vladimir Ivanov
+
+;; Author: Vladimir Ivanov <ivvl82@gmail.com>
+;; Keywords:
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;; Disable tick-tack sound
+(setq pomidor-sound-tick nil
+      pomidor-sound-tack nil)
+
+;;; init-pomidor.el ends here
