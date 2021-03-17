8a27b2597448fb1e5c40b66af6c06a6caa6fd081
Vladimir Ivanov <ivvl82@gmail.com>
2021-03-17 21:42:37 +0300

    Allow to group El-Get package list.

--- init-files/init-el-get.el
+++ init-files/init-el-get.el
@@ -1,6 +1,6 @@
 ;;; init-el-get.el ---
 
-;; Copyright (C) 2015, 2016  Vladimir Ivanov
+;; Copyright (C) 2015-2021  Vladimir Ivanov
 
 ;; Author: Vladimir Ivanov <ivvl82@gmail.com>
 ;; Keywords:
@@ -17,25 +17,24 @@
   "A list of built-in packages, initialization files for which
   are loaded at launch.")
 (defvar my:el-get-packages
-  '(ace-jump-mode           ; fast buffer switching extension to `avy'
-    adaptive-wrap           ; smart line-wrapping with wrap-prefix
-    auctex                  ; integrated environment for *TeX*
-    essh    ; a set of commands that emulate for bash what ESS is to R
-    fill-column-indicator       ; graphically indicate the fill column
-    fullscreen                  ; full screen
-    hungry-delete               ; hungry delete minor mode
-    magit                       ; a Git porcelain inside Emacs
-    nlinum                      ; show line numbers in the margin
-    org     ; outline-based notes management and organizer
-    pomidor ; simple and cool http://www.pomodorotechnique.com timer
-    realgud ; a modular front-end for interacting with external debuggers
-    selectrum                           ; easily select item from list
-    selectrum-prescient                 ; Selectrum integration
-    sdcv             ; interface for sdcv (StartDict console version)
-    solarized-emacs  ; Solarized for Emacs
-    undo-tree        ; treat undo history as a tree
-    window-margin    ; automatic margins for visual-line-mode wrapping
-    ws-butler)       ; unobtrusively remove trailing whitespace
+  '(ace-jump-mode     ; fast buffer switching extension to `avy'
+    adaptive-wrap     ; smart line-wrapping with wrap-prefix
+    auctex            ; integrated environment for *TeX*
+    essh              ; a set of commands that emulate for bash what ESS is to R
+    fill-column-indicator         ; graphically indicate the fill column
+    fullscreen                    ; full screen
+    hungry-delete                 ; hungry delete minor mode
+    magit                         ; a Git porcelain inside Emacs
+    nlinum                        ; show line numbers in the margin
+    org                           ; outline-based notes management and organizer
+    pomidor             ; simple and cool http://www.pomodorotechnique.com timer
+    (selectrum          ; easily select item from list
+     selectrum-prescient)      ; Selectrum integration
+    sdcv                       ; interface for sdcv (StartDict console version)
+    solarized-emacs            ; Solarized for Emacs
+    undo-tree                  ; treat undo history as a tree
+    window-margin              ; automatic margins for visual-line-mode wrapping
+    ws-butler)                 ; unobtrusively remove trailing whitespace
   "A list of packages to ensure are installed at launch.")
 
 ;; Load initialization files for built-in packages
@@ -47,7 +46,15 @@
   (dolist (file init-files)
     (load-file (expand-file-name file el-get-user-package-directory))))
 
-(el-get-cleanup my:el-get-packages)
-(el-get 'sync my:el-get-packages)
+;; Clean up and init packages installed by El-Get
+(let* ((flatten (lambda (l)
+                  (if (listp l)
+                      (apply 'concatenate
+                             'list
+                             (mapcar (lambda (p) (funcall flatten p)) l))
+                    (list l))))
+       (packages (funcall flatten my:el-get-packages)))
+  (el-get-cleanup packages)
+  (el-get 'sync packages))
 
 ;;; init-el-get.el ends here
