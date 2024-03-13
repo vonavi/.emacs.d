429c56c516975e723da4dbaf101d36ed7bdaa8b0
Vladimir Ivanov <ivvl82@gmail.com>
2024-03-13 16:32:27 +0800

    Install color-oriented packages by means of 'use-package'.

---
+++ packages/config/rainbow-delimiters.el
@@ -0,0 +1,7 @@
+;;; rainbow-delimiters.el
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(provide 'config/rainbow-delimiters)
+;;; rainbow-delimiters.el ends here
