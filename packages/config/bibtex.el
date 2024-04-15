3cbbfe2287b87d9cbae603c279aa0476e38ec8b5
Vladimir Ivanov <ivvl82@gmail.com>
2024-04-15 07:58:14 +0300

    Use org-directory in bibtex-completion variables.

--- packages/config/bibtex.el
+++ packages/config/bibtex.el
@@ -2,10 +2,15 @@
 
 (use-package bibtex
   :ensure nil
+  :after org
   :init
   ;; Set bibtex-completion variables
-  (setq bibtex-completion-bibliography '("~/org/bibliography/references.bib")
-        bibtex-completion-library-path '("~/org/bibliography/bibtex-pdfs/")))
+  (setq bibtex-completion-bibliography
+        `(,(concat (file-name-as-directory org-directory)
+                   "bibliography/references.bib"))
+        bibtex-completion-library-path
+        `(,(concat (file-name-as-directory org-directory)
+                   "bibliography/bibtex-pdfs/"))))
 
 (provide 'config/bibtex)
 ;;; bibtex.el ends here
