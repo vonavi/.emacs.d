4310b21b79ea9386ef8e6ef86a9ff1551eaad089
Vladimir Ivanov <ivvl82@gmail.com>
2024-04-18 15:29:17 +0300

    Add key bindings for Org-roam.

--- packages/config/org-roam.el
+++ packages/config/org-roam.el
@@ -1,6 +1,7 @@
 ;;; org-roam.el
 
 (use-package org-roam
+  :ensure-system-package (dot . graphviz)
   :after org
   :init
   (setq org-roam-directory     ; directory to store the Org-roam files
@@ -8,7 +9,12 @@
   ;; Show the tags associated with the note
   (setq org-roam-node-display-template
         (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
   :config
   ;; Update SQLite database of Org files as they are modified
   (org-roam-db-autosync-mode +1))
