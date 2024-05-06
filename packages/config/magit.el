e1dcb3e76aae3331b755961e6767ec24f8688f59
Vladimir Ivanov <ivvl82@gmail.com>
2024-05-06 11:27:23 +0300

    Improve the window management for Magit buffers.

--- packages/config/magit.el
+++ packages/config/magit.el
@@ -5,6 +5,9 @@
   :init
   ;; Show fine (word-granularity) differences within diff hunks
   (setq magit-diff-refine-hunk 'all)
+  ;; Open Magit full-frame then restore windows on quit
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
+        magit-bury-buffer-function #'magit-restore-window-configuration)
 
   ;; Magit's status buffer
   :bind ("C-x m" . magit-status)
