42629ae4da1527d7b94061329858953df94fa1d5
Vladimir Ivanov <ivvl82@gmail.com>
2024-05-07 11:11:04 +0300

    Update how line numbers are displayed.

--- packages/config/display-line-numbers.el
+++ packages/config/display-line-numbers.el
@@ -1,8 +1,10 @@
 ;;; display-line-numbers.el
 
 (use-package display-line-numbers
-  ;; Avoid the annoying shifting of line number width
-  :init (setq-default display-line-numbers-width 4)
+  :init
+  (setq-default
+   display-line-numbers-widen t  ; use absolute numbers in narrowed buffers
+   display-line-numbers-width 3) ; less jitter when scrolling
   :hook (prog-mode . (lambda () (display-line-numbers-mode +1))))
 
 (provide 'config/display-line-numbers)
