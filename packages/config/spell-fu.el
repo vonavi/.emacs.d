d9029238aadcf948065fa3ad224659512dafa678
Vladimir Ivanov <ivvl82@gmail.com>
2024-05-08 13:43:53 +0300

    Configure Spell Fu per-mode.

--- packages/config/spell-fu.el
+++ packages/config/spell-fu.el
@@ -2,13 +2,23 @@
 
 (use-package spell-fu
   :ensure-system-package aspell
-  :config
-  (spell-fu-global-mode +1)
+  :after org
   :hook
-  ;; Use multiple languages in the same buffer
-  (spell-fu-mode . (lambda ()
-                     (spell-fu-dictionary-add
-                      (spell-fu-get-ispell-dictionary "ru")))))
+  ((spell-fu-mode . (lambda ()
+                      ;; Use multiple languages in the same buffer
+                      (spell-fu-dictionary-add
+                       (spell-fu-get-ispell-dictionary "ru"))))
+   ((prog-mode text-mode) . (lambda ()
+                              (spell-fu-mode +1)))
+   (org-mode . (lambda ()
+                 (setq spell-fu-faces-exclude
+                       '(org-block
+                         org-block-begin-line
+                         org-block-end-line
+                         org-code
+                         org-meta-line
+                         org-special-keyword))
+                 (spell-fu-mode +1)))))
 
 (provide 'config/spell-fu)
 ;;; spell-fu.el ends here
