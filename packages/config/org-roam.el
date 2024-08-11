cb559c3b23e7f35acf0f26d658ca3891f031b3cb
Vladimir Ivanov <ivvl82@gmail.com>
2024-08-11 18:47:24 +0300

    Add Org-roam specific key-bindings into 'org-mode-map'.

--- packages/config/org-roam.el
+++ packages/config/org-roam.el
@@ -9,12 +9,18 @@
   ;; Show the tags associated with the note
   (setq org-roam-node-display-template
         (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ("C-c n j" . org-roam-dailies-capture-today))
+  :bind
+  (("C-c n f" . org-roam-node-find)
+   ("C-c n g" . org-roam-graph)
+   ("C-c n c" . org-roam-capture)
+   ("C-c n j" . org-roam-dailies-capture-today)
+   :map org-mode-map
+   ("C-c n t" . org-roam-tag-add)
+   ("C-c n T" . org-roam-tag-remove)
+   ("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n r" . org-roam-refile)
+   ("C-c n x" . org-roam-extract-subtree))
   :config
   ;; Update SQLite database of Org files as they are modified
   (org-roam-db-autosync-mode +1))
