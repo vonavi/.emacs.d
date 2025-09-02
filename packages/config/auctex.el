62b7376c7c7f86a5cc65a94e192879f7db951181
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-02 17:59:12 +0300

    Set variables using keyword :custom in config/auctex.el.

--- packages/config/auctex.el
+++ packages/config/auctex.el
@@ -3,19 +3,17 @@
 (use-package tex
   :ensure auctex
   :ensure-system-package (pdflatex . texlive-latex-base)
-  :init
-  (setq
-   ;; Give TeX permission to call foreign programs
-   TeX-command-extra-options "-shell-escape"
-   ;; Insert paired symbols for opening and closing inline equation
-   TeX-electric-math '("$" . "$")
-   ;; Parse the buffer on load for extracting information
-   TeX-parse-self t
-   ;; Auto save before compiling
-   TeX-save-query nil
-   ;; Start a correlation server without asking
-   TeX-source-correlate-start-server t)
-
+  :custom
+  ;; Give TeX permission to call foreign programs
+  (TeX-command-extra-options "-shell-escape")
+  ;; Insert paired symbols for opening and closing inline equation
+  (TeX-electric-math '("$" . "$"))
+  ;; Parse the buffer on load for extracting information
+  (TeX-parse-self t)
+  ;; Auto save before compiling
+  (TeX-save-query nil)
+  ;; Start a correlation server without asking
+  (TeX-source-correlate-start-server t)
   :hook
   (TeX-mode . (lambda ()
                 ;; Folding macros and environments
@@ -30,21 +28,19 @@
 
 (use-package latex
   :ensure auctex
-  :init
-  (setq
-   ;; Function for reading \includegraphics files
-   LaTeX-includegraphics-read-file 'LaTeX-includegraphics-read-file-relative
-   ;; Strip known extensions from image file name
-   LaTeX-includegraphics-strip-extension-flag nil)
-
+  :custom
+  ;; Function for reading \includegraphics files
+  (LaTeX-includegraphics-read-file 'LaTeX-includegraphics-read-file-relative)
+  ;; Strip known extensions from image file name
+  (LaTeX-includegraphics-strip-extension-flag nil)
   ;; Fold a math macro automatically after it's inserted
-  (setq LaTeX-math-insert-function
-        (lambda (string)
-          (TeX-insert-macro string)
-          (save-excursion (backward-char) (TeX-fold-item 'math))))
-
+  (LaTeX-math-insert-function (lambda (string)
+                                (TeX-insert-macro string)
+                                (save-excursion (backward-char)
+                                                (TeX-fold-item 'math))))
+  :hook
   ;; Easy typing of mathematical symbols
-  :hook (LaTeX-mode . (lambda () (LaTeX-math-mode +1))))
+  (LaTeX-mode . (lambda () (LaTeX-math-mode +1))))
 
 (use-package auctex-latexmk
   :ensure-system-package latexmk
@@ -52,37 +48,35 @@
 
 (use-package reftex
   :ensure nil
+  :custom
+  ;; Make RefTeX work properly with AUCTeX
+  (reftex-plug-into-AUCTeX t)
+  ;; Add \eqref to RefTeX for doing equation references
+  (reftex-label-alist '(AMSTeX))
+  ;; Additional key bindings for RefTeX mode
+  (reftex-extra-bindings t)
+  ;; Auto insert a tilde before the \cite macro if the preceding
+  ;; character isn't whitespace or a tilde
+  (reftex-format-cite-function
+   (lambda (key fmt)
+     (concat
+      (unless (or
+               ;; We are inside a cite command
+               (some (apply-partially 'string= fmt)
+                     '("%l" ",%l" "%l," ",%l,"))
+               ;; There is already a tilde
+               (= ?~ (string-to-char fmt))
+               ;; The preceding character is a whitespace or tilde
+               (member (preceding-char) '(?\ ?\t ?\n ?~)))
+        "~")
+      (replace-regexp-in-string "%l" key fmt))))
   :init
-  (setq
-   ;; Make RefTeX work properly with AUCTeX
-   reftex-plug-into-AUCTeX t
-   ;; Add \eqref to RefTeX for doing equation references
-   reftex-label-alist '(AMSTeX)
-   ;; Additional key bindings for RefTeX mode
-   reftex-extra-bindings t)
-
   ;; Fold a RefTeX macro automatically after it's inserted
   (defun my/RefTeX-fold ()
     (save-excursion (backward-char) (TeX-fold-item 'macro)))
   (advice-add 'reftex-label :after #'my/RefTeX-fold)
   (advice-add 'reftex-reference :after #'my/RefTeX-fold)
   (advice-add 'reftex-citation :after #'my/RefTeX-fold)
-
-  ;; Auto insert a tilde before the \cite macro if the preceding
-  ;; character isn't whitespace or a tilde
-  (setq reftex-format-cite-function
-        (lambda (key fmt)
-          (concat
-           (unless (or
-                    ;; We are inside a cite command
-                    (some (apply-partially 'string= fmt)
-                          '("%l" ",%l" "%l," ",%l,"))
-                    ;; There is already a tilde
-                    (= ?~ (string-to-char fmt))
-                    ;; The preceding character is a whitespace or tilde
-                    (member (preceding-char) '(?\ ?\t ?\n ?~)))
-             "~")
-           (replace-regexp-in-string "%l" key fmt))))
 
   ;; Support for labels, references, citations and index entries
   :hook (TeX-mode . turn-on-reftex)
