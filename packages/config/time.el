c8773ee063d98bad696e9f8cac305cb0e97410c3
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-02 16:16:20 +0300

    Set variables using keyword :custom in config/time.el.

--- packages/config/time.el
+++ packages/config/time.el
@@ -2,11 +2,11 @@
 
 (use-package time
   :ensure nil
-  :init
-  (setq display-time-day-and-date t ; display the day and date in the mode line
-        display-time-24hr-format t  ; use 24hr format
-        display-time-interval 10    ; redisplay every ten seconds
-        display-time-default-load-average nil) ; don't display load average
+  :custom
+  (display-time-day-and-date t) ; display the day and date in the mode line
+  (display-time-24hr-format t)  ; use 24hr format
+  (display-time-interval 10)    ; redisplay every ten seconds
+  (display-time-default-load-average nil) ; don't display load average
   :config
   (display-time-mode +1))
 
