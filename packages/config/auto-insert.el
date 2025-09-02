7dcf168ed13e6fce742577be94047cf6343c322a
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-02 16:44:36 +0300

    Set variables using keyword :custom in config/auto-insert.el.

--- packages/config/auto-insert.el
+++ packages/config/auto-insert.el
@@ -6,16 +6,16 @@
 (use-package autoinsert
   :ensure nil
   :ensure-system-package git
-  :init
-  (setq auto-insert-query nil          ; don't prompt before insertion
-        auto-insert 'other) ; insert if possible, but mark as unmodified
+  :custom
+  (auto-insert-query nil) ; don't prompt before insertion
+  (auto-insert 'other)    ; insert if possible, but mark as unmodified
   ;; User information to be auto-inserted
-  (setq user-full-name
-        (string-trim-right (shell-command-to-string "git config user.name"))
-        user-mail-address
-        (string-trim-right (shell-command-to-string "git config user.email")))
-
-  :config (auto-insert-mode +1))
+  (user-full-name
+   (string-trim-right (shell-command-to-string "git config user.name")))
+  (user-mail-address
+   (string-trim-right (shell-command-to-string "git config user.email")))
+  :config
+  (auto-insert-mode +1))
 
 (provide 'config/auto-insert)
 ;;; auto-insert.el ends here
