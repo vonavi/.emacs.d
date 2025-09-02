c6e306406c966208ce16dd6c74759214a032ee79
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-02 15:18:28 +0300

    Set variables using keyword :custom in config/undo-tree.el.

--- packages/config/undo-tree.el
+++ packages/config/undo-tree.el
@@ -2,12 +2,14 @@
 
 (use-package undo-tree
   :delight
-  :init
-  (setq undo-tree-auto-save-history t   ; save history to a file
-        ;; Back up all undo-history files into one directory
-        undo-tree-history-directory-alist
-        `(("." . ,(expand-file-name "undo/" user-emacs-directory))))
-  :config (global-undo-tree-mode +1))
+  :custom
+  ;; Save history to a file
+  (undo-tree-auto-save-history t)
+  ;; Back up all undo-history files into one directory
+  (undo-tree-history-directory-alist
+   `(("." . ,(expand-file-name "undo/" user-emacs-directory))))
+  :config
+  (global-undo-tree-mode +1))
 
 (provide 'config/undo-tree)
 ;;; undo-tree.el ends here
