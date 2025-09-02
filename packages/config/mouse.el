5af7ba6ab162cffd39a5e6bba81f7a872cad999e
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-02 17:04:29 +0300

    Set variables using keyword :custom in config/mouse.el.

--- packages/config/mouse.el
+++ packages/config/mouse.el
@@ -2,14 +2,14 @@
 
 (use-package mouse
   :ensure nil
-  :init (setq mouse-yank-at-point t))   ; paste at point NOT at cursor
+  :custom (mouse-yank-at-point t))      ; paste at point NOT at cursor
 
 (use-package mwheel
   :ensure nil
-  :init
-  (setq mouse-wheel-scroll-amount '(1) ; mouse scroll one line at a time
-        mouse-wheel-progressive-speed t ; don't accelerate scrolling
-        mouse-wheel-follow-mouse t)     ; scroll window under mouse
+  :custom
+  (mouse-wheel-scroll-amount '(1))   ; mouse scroll one line at a time
+  (mouse-wheel-progressive-speed t)  ; don't accelerate scrolling
+  (mouse-wheel-follow-mouse t)       ; scroll window under mouse
   :config
   (mouse-wheel-mode +1))
 
