16a5abc5ba700ad2e131ff846d1456c91f32e177
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-03 14:32:29 +0300

    Set variables using keyword :custom in config/citar.el.

--- packages/config/citar.el
+++ packages/config/citar.el
@@ -2,27 +2,29 @@
 
 (use-package citar
   :after (bibtex oc)
-  :init
-  (setq citar-bibliography bibtex-completion-bibliography
-        citar-library-paths bibtex-completion-library-path)
+  :custom
+  (citar-bibliography bibtex-completion-bibliography)
+  (citar-library-paths bibtex-completion-library-path)
   ;; Select citation processors
-  (setq org-cite-activate-processor 'citar
-        org-cite-follow-processor 'citar
-        org-cite-insert-processor 'citar)
+  (org-cite-activate-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-insert-processor 'citar)
   ;; How to open files with given extensions
-  (setq citar-file-open-functions '(("html" . citar-file-open-external)
-                                    ("pdf" . citar-file-open-external)
-                                    (t . find-file))))
+  (citar-file-open-functions '(("html" . citar-file-open-external)
+                               ("pdf" . citar-file-open-external)
+                               (t . find-file))))
 
 (use-package citar-org-roam
   :after org-roam
   :delight
-  :init
-  (setq citar-org-roam-note-title-template ; the default note title output
-        "${author editor:%etal} (${year issued date}) ${title}"
-        citar-org-roam-subdir      ; the directory of literature notes
-        "references/")
-  :config (citar-org-roam-mode +1))
+  :custom
+  ;; Default note title output
+  (citar-org-roam-note-title-template
+   "${author editor:%etal} (${year issued date}) ${title}")
+  ;; Directory of literature notes
+  (citar-org-roam-subdir "references/")
+  :config
+  (citar-org-roam-mode +1))
 
 (provide 'config/citar)
 ;;; citar.el ends here
