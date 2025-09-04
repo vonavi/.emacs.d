84a4abc28b1e4660837ea29c1b88c6ed4ad0de48
Vladimir Ivanov <ivvl82@gmail.com>
2025-09-04 14:44:25 +0300

    Automatically kill running processes on exit

--- packages/config/files.el
+++ packages/config/files.el
@@ -3,6 +3,8 @@
 (use-package files
   :ensure nil
   :custom
+  ;; Automatically kill running processes on exit
+  (confirm-kill-processes nil)
   ;; Don't litter my fs tree
   (backup-directory-alist
    `(("." . ,(expand-file-name "backup/" user-emacs-directory))))
