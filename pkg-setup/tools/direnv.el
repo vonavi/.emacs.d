372e0b1ca256275fa5a1c6540f9050d8cd43c389
Vladimir Ivanov <ivvl82@gmail.com>
2026-03-22 19:05:34 +0300

    Fix the execution of Org Babel blocks for Org export.

--- pkg-setup/tools/direnv.el
+++ pkg-setup/tools/direnv.el
@@ -8,9 +8,10 @@
   :ensure-system-package direnv
   :hook (after-init . envrc-global-mode)
   :config
-  ;; Ensure babel's execution environment matches the host buffer's
-  (advice-add #'org-babel-execute-src-block
-              :around #'envrc-propagate-environment))
+  ;; Ensure Org Babel's execution environment matches the host buffer's
+  (with-eval-after-load 'ox
+    (advice-add #'org-export-as
+                :around #'envrc-propagate-environment)))
 
 (provide 'tools/direnv)
 ;;; tools/direnv.el ends here
